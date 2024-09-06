48c44150b8808418c70477c6a9827b9a8f701283
Han Yu Chiang <65708607+Shiba0228@users.noreply.github.com>
2024-09-06 22:57:52 +0800

    Add files via upload

---
+++ uva11150.cpp
@@ -0,0 +1,25 @@
+// UVa 11150 - Cola
+#include <iostream>
+using namespace std; 
+
+int main() {
+    int n;
+    cin>>n; 
+        int total = n;  
+        int bottles = n;  
+
+        while (bottles >= 3) {
+            int new_cola = bottles / 3;
+            total += new_cola;
+            bottles = bottles % 3 + new_cola;
+        }
+
+        
+        if (bottles == 2) {
+            total += 1;
+        }
+
+        cout<<total<<endl;
+
+    return 0;
+	}1
