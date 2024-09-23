bb9a1e3f35935035414d8f0ee06c146fb1be2fa6
Han Yu Chiang <65708607+Shiba0228@users.noreply.github.com>
2024-09-23 19:25:49 +0800

    Update uva10071.cpp

--- uva10071.cpp
+++ uva10071.cpp
@@ -1,9 +1,27 @@
+//back to high school physic
 #include<iostream>
-using namespace std;
-int main(){
-	int v,t,d;
-	while(cin>>v >>t){
-		d=v*t*2;
-		cout<<d<<endl;
-	}
+using  namespace std;
+
+int distance(int v, int t){
+   return 2*v*t;
 }
+int main(){
+   int v, t;
+   while(cin>>v >>t)
+      cout<<distance(v,t)<<endl;
+   return 0;
+}
+
+//version2(瘋狂程社會有bug)
+//#include<iostream>
+//using  namespace std;
+//int main(){
+  // int v, t;  
+  // while(cin>>v >>t){
+   //   int  d;
+   //  d=2*v*t;
+   //  cout<<d <<endl;
+   // }  
+   //return 0;
+   
+//}
