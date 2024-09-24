88c449af99ad6b51c8c18b9ae511e2002e41c9a3
Han Yu Chiang <65708607+Shiba0228@users.noreply.github.com>
2024-09-24 20:29:01 +0800

    Update uva10101.cpp

--- uva10101.cpp
+++ uva10101.cpp
@@ -1,48 +1,61 @@
-//Bangla Numbers
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
-void Bangla(long long int n)
+
+const long long kuti = 10000000, lakh = 100000, hajar = 1000, shata = 100;
+
+void processBangla(long long num)
 {
-    while(n>0)
+    if (num >= kuti)
     {
-        if(n>=10000000) {
-            cout<<" "<<n/10000000<<" kuti";
-            n%=10000000;
-            continue; }
-        if(100000<=n&&n<10000000) {
-            cout<<" "<<n/100000<<" lakh";
-            n%=100000;
-            continue; }
-        if(1000<=n&&n<100000) {
-            cout<<" "<<n/1000<<" hajar";
-            n%=1000;
-            continue; }
-        if(100<=n&&n<1000) { 
-            cout<<" "<<n/100<<" shata";
-            n%=100;
-            continue; }
-        else {
-            cout<<" "<<n;
-            break; }
+        processBangla(num / kuti);
+        cout << " kuti";
+        num %= kuti;
+    }
+    if (num >= lakh)
+    {
+        cout << " " << num / lakh << " lakh";
+        num %= lakh;
+    }
+    if (num >= hajar)
+    {
+        cout << " " << num / hajar << " hajar";
+        num %= hajar;
+    }
+    if (num >= shata)
+    {
+        cout << " " << num / shata << " shata";
+        num %= shata;
+    }
+    if (num > 0)
+    {
+        cout << " " << num;
     }
 }
 
 int main()
 {
-    long long int n;
-    int T=1;
-    while(cin>>n)
+    long long num;
+    int case_count = 1;
+
+    while (cin >> num)
     {
-        cout<<setw(4)<<setfill(' ')<<T<<".";
-        if(n==0) cout<<" "<<0;
-        if(n>=10000000) {
-            Bangla(n/10000000);
-            cout<<" kuti";
-            Bangla(n%10000000);}
-        else {
-            Bangla(n);}
-        cout<<"\n";
-        T++;
+        for (int i = 0; i < 4 - to_string(case_count).length(); i++)
+        {
+            cout << " ";
+        };
+        cout << case_count << ".";
+        if (num == 0)
+        {
+            cout << " 0";
+        }
+        else
+        {
+            processBangla(num);
+        }
+        cout << endl;
+        case_count++;
     }
+
     return 0;
 }
