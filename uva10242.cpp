037d71aaa0664727f251567a14fce9d1af628c2d
Han Yu Chiang <65708607+Shiba0228@users.noreply.github.com>
2024-10-08 15:59:13 +0800

    Update uva10242.cpp

--- uva10242.cpp
+++ uva10242.cpp
@@ -1,10 +1,11 @@
 //four points
 //平行四邊形
+//給定平行四邊形的兩個相鄰邊的端點的 (x, y)座標。找到第四個點的 (x, y)座標
 #include <iostream>
 #include <iomanip>  //輸出格式
 using namespace std;
 
-// 解決問題的函數
+// four points function
 void solve(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4) {
     if (x1 == x3 && y1 == y3) {
         cout << fixed << setprecision(3) << (x2 + x4) - x1 << " " << (y2 + y4) - y1 << endl;
@@ -16,7 +17,7 @@ void solve(double x1, double y1, double x2, double y2, double x3, double y3, dou
         cout << fixed << setprecision(3) << (x1 + x3) - x2 << " " << (y1 + y3) - y2 << endl;
     }
 }
-
+//main function
 int main() {
     double x1, y1, x2, y2, x3, y3, x4, y4;
     while (cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4) {
