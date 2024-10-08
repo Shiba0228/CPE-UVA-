425603f0083155b45121c605a5d92ff3e63e3fb4
Han Yu Chiang <65708607+Shiba0228@users.noreply.github.com>
2024-10-08 15:38:18 +0800

    Update uva10190.cpp

--- uva10190.cpp
+++ uva10190.cpp
@@ -8,17 +8,17 @@ using namespace std;
 int main() {
     long long n, m, i;
 
-    while (cin >> n >> m) {
+    while (cin >> n >> m) { //input
         long long cn1 = m;
 
-        for (i = 1; cn1 < n && m > 1; i++) {
+        for (i = 1; cn1 < n && m > 1; i++) { //計算n是否為m的次方數 
             cn1 = pow(m, i);
         }
 
-        if (cn1 > n || m <= 1) {
+        if (cn1 > n || m <= 1) {//不是次方數
             cout << "Boring!" << endl;
         } else {
-            while (cn1 > 0) {
+            while (cn1 > 0) {//從n開始輸出m次方數 
                 cout << cn1;
                 if (cn1 != 1) {
                     cout << " ";
