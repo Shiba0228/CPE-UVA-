29d2af92f70eb780a7d422a2f45bffb4f0ee3e52
Han Yu Chiang <65708607+Shiba0228@users.noreply.github.com>
2024-10-08 15:49:21 +0800

    Update uva10221.cpp

--- uva10221.cpp
+++ uva10221.cpp
@@ -1,4 +1,9 @@
 //Satellites
+//Degrees (°), minutes (‘), seconds (“), 1° = 60′,1′ = 60″
+//角度 degree (deg) vs. 弧度 radius (rad),一個完整的圓的弧度：2 * PI, rad = deg / 180 * PI, cos (90°) = 0 -> acos(0) = PI / 2 -> PI = 2 * acos(0)
+//一個半徑為 R 的圓，圓周長為 2 * PI * R，當兩顆衛星之間的夾角「角度」為 a°（a 大於 180 時，a -> 360 – a）
+//弧長 arc = 2 * PI * R * a / 360
+//弦長 chord = 2* R * cos((90 – a / 2) / 180 * PI)
 #include <iostream>
 #include <iomanip>
 #include <cmath>
