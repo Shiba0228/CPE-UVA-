203a6b68cb2f758ca0198b8e645d345ac9532732
Han Yu Chiang <65708607+Shiba0228@users.noreply.github.com>
2024-10-08 19:56:16 +0800

    Update uva10325.cpp

--- uva10325.cpp
+++ uva10325.cpp
@@ -1,10 +1,12 @@
 //Simply Emirp
+//GCD, LCM
 #include <iostream>
 using namespace std;
 int p[1000005];
  
 int main() {
     p[1] = 1;
+    //使用篩選法（埃拉托斯特尼篩法）來標記所有非質數
     for (int i = 2; i < 1000005; i++){
         if (p[i] == 0){
             for (int j = i+i; j < 1000005; j+=i){
@@ -13,6 +15,11 @@ int main() {
         }
     }
     int N, N1, N2;
+ //從標準輸入中讀取一個整數 N 並進行以下操作
+ //將 N 的反向數存入 N2。例如，123 會轉換為 321
+ //如果 N 是非質數 (p[N] 為 1)，輸出 N is not prime.
+ //如果 N 是質數，且反向數 N2 不等於 N 並且 N2 也是質數，則輸出 N is emirp.。反質數（emirp）是指一個數及其反向數均為質數且不相等。
+ //否則，僅輸出 N is prime.，表示 N 是質數但不是反質數。
     while (cin >> N){
         N1 = N;
         N2 = 0;
