2bb41ea3fe403dd07cfe0c7e1b03b05f2dbfba83
Han Yu Chiang <65708607+Shiba0228@users.noreply.github.com>
2024-10-09 14:19:11 +0800

    Update uva11005.cpp

--- uva11005.cpp
+++ uva11005.cpp
@@ -4,7 +4,10 @@
 #include <cstdio>
 
 using namespace std;
-
+//function f:將十進制數字 a 轉換為進制 b 表示的數字，並存儲在 arr 中
+//使用 tmp[] 暫存轉換後的每一位（從低位到高位）。
+//通過 a % b 取餘數，得到在進制 b 下的最低位，然後除以 b 繼續處理，直到 a < b
+//最後將 tmp 中的數字逆序存入 arr，因為 tmp 存儲的是從低位到高位，而 arr 需要從高位到低位。
 int f(int a, int b, int arr[], int &cnt) {
 	int tmp[40] = {}, i;
 	cnt = 0;
@@ -19,7 +22,8 @@ int f(int a, int b, int arr[], int &cnt) {
 
 	return 0;
 }
-
+//function calcost: 計算將數字 num 轉換為進制 b 表示時的總成本。
+//遍歷 arr[] 中的每一位數字，將每一位的轉換成本累加，得到總成本 sum。
 int CalCost(int arr[], int cnt, int cost[]) {
 	int sum = 0;
 	for(int i = 0; i < cnt; ++i) {
@@ -28,6 +32,13 @@ int CalCost(int arr[], int cnt, int cost[]) {
 
 	return sum;
 }
+//main function:理多個測試用例，對每個測試用例找出數字 num 的最便宜轉換進制。
+//讀取輸入：讀取 t 和每個測試用例的 cost[]，表示在不同進制下，每個數字的成本。對每個測試用例，讀取 n 和需要判斷的 num。
+//找最便宜的進制：
+//對於每個 num，遍歷進制 b（從 2 到 36）。
+//使用 f() 函數將 num 轉換為進制 b，並計算其轉換成本。
+//使用 CalCost() 計算轉換後的總成本，儲存在 price[b] 中。
+//更新 Min，以找到最小成本的進制。
 
 int main()
 {
