a737683cdfc1ab2a56f7c9c5787b8e8de01aecb8
Han Yu Chiang <65708607+Shiba0228@users.noreply.github.com>
2024-10-13 18:05:37 +0800

    Update uva10035.cpp

--- uva10035.cpp
+++ uva10035.cpp
@@ -1,4 +1,8 @@
 //Primary Arithmetic
+//在小學時我們都做過加法的運算，就是把2個整數靠右對齊然後，由右至左一位一位相加。如果相加的結果大於等於10就有進位（carry）的情況出現。你的任務就是要判斷2個整數相加時產生了幾次進位的情況。這將幫助小學老師分析加法題目的難度。
+//計算輸入值的各個位數相加是否進位，若進位記得將加入下一個位數計算（注意：輸入值使用 unsigned long long int）
+//判斷各輸出格式
+//（注意：carry 超過 1 時，operation 要加s）
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
